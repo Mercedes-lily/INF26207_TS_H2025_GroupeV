0ebf99947af72fca4770d81f93b94827db326c18
Valerie2241 <122388760+Valerie2241@users.noreply.github.com>
2025-03-05 12:48:24 -0500

    Ajouter un main et ajouter le fichier .txt

--- Client/client.py
+++ Client/client.py
@@ -1,1 +1,18 @@
 #Fichiers de bases du coté Client
+
+#Pour executer le programme : python cheminVerLeFichier
+
+
+if __name__ == '__main__':
+
+    # function commandels(){} qui recupere les fichier dans un dossier et qui les enregistre dans une list ou un tabeau
+
+    # Demander au client de faire open avec une adresse ip
+    # Verifier que le client a entrer open avec quelchose apres (Ex: open 127.0.0.1)
+    # et non (open) ou (open 127.0.0.1 127.0.0.2)
+
+    
+    print("Allo")
+    str=input("Entrer votre nom : ")
+    print("--"+str+"--")
+
