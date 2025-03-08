8e1ba846828f4e0e7fa604a9b7100ac10e36ec60
Valerie2241 <122388760+Valerie2241@users.noreply.github.com>
2025-03-08 18:02:33 -0500

    3 way minimaliste fonctionel

--- Client/client.py
+++ Client/client.py
@@ -1,2 +1,24 @@
 #Fichiers de bases du coté Client
+#Librairie pour utiliser les sockets
+import socket   
 
+#Initialisation du côté client et implémentation du three-Way Handshake
+def SocketStart():
+    serveur_port = 2212
+    serveur_adresse = "127.0.0.1"
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    message = "SYN"
+    client_socket.sendto(message.encode(), (serveur_adresse, serveur_port))
+    client_socket.settimeout(5)
+    data, serv_adresse = client_socket.recvfrom(1024)  # Receive up to 1024 bytes
+    print(f"Received data from {serv_adresse}: {data.decode()}") #decodes the recieved bytes.
+    if data.decode() == "SYN-ACK":
+        message = "ACK"  
+        client_socket.sendto(message.encode(), serv_adresse)
+
+def main():
+    SocketStart()
+    
+
+if __name__ == "__main__":
+    main()
--- Serveur/myFTPserveur.py
+++ Serveur/myFTPserveur.py
@@ -1,6 +1,9 @@
 #Fichiers de bases du coté Serveur
+#Librairie pour utiliser les sockets
 import socket   
 
+
+#Initialisation du socket UDP du côté serveur et implémentation du three-Way Handshake
 def ServeurStart():
     port = 2212
     adresse = "127.0.0.1"
@@ -11,19 +14,28 @@ def ServeurStart():
     except Exception as e:
         print("Bind : Echec, error: " + e)
         exit()
-
-    try:
-        data, client_adresse = serv_socket.recvfrom(1024)  # Receive up to 1024 bytes
-        print(f"Received data from {client_adresse}: {data.decode()}") #decodes the recieved bytes.
-        print("Receive : Succes")
-
-    except Exception as e:
-        print(f"Receive : Echec, error: {e}")
-        exit()
+    serv_socket.settimeout(5)
+    data, client_adresse = serv_socket.recvfrom(1024)  # Receive up to 1024 bytes
+    data_decode = data.decode()
+    print(f"Received data from {client_adresse}: {data_decode}") #decodes the recieved bytes.
+    print(data_decode == "SYN")
+    print("--" + data_decode + "--")
+    if data_decode == "SYN":
+        message = "SYN-ACK"  
+        serv_socket.sendto(message.encode(), client_adresse)
+    data, client_adresse = serv_socket.recvfrom(1024)  # Receive up to 1024 bytes
+    print(f"Received data from {client_adresse}: {data.decode()}") #decodes the recieved bytes.
+    if data.decode() == "ACK":
+        print("3 way : reussi")
+    
+    return serv_socket
     
 
 def main():
-    ServeurStart()
+    print("L'adresse ipv4 de se serveur est : 127.0.0.1")
+    serv_socket = ServeurStart()
+
+
     
 
 if __name__ == "__main__":
